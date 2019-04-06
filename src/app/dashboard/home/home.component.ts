d95ddd754bbc4d695d331e0463d5c16ce9486b96
Love Chaudhary <lovechaudhary17@gmail.com>
2019-04-06 19:52:21 -0400

    add search and category tag on home page

--- src/app/dashboard/home/home.component.ts
+++ src/app/dashboard/home/home.component.ts
@@ -9,7 +9,11 @@ import { ActivatedRoute, Router } from "@angular/router";
   styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
-  public isLoading: boolean;
+  public isLoading = false;
+  public isCategory = false;
+  public isSearch = false;
+  public searchString: string;
+  public categoryString: string;
   month: string;
   day: number;
   year: number;
@@ -52,38 +56,52 @@ export class HomeComponent implements OnInit {
         countryName = "us";
         for (let field in param) {
           if (field === "query") {
+            this.isCategory = false;
+            this.isSearch = true;
+            this.searchString = param.query;
             this._newsService.getSearchedNews(param.query).subscribe(
               news => {
                 this._zone.run(() => {
                   this.news = news.articles;
                 });
+                this.isLoading = false;
               },
               error => {
                 if (error) {
                   console.log(error);
                 }
+                this.isLoading = false;
               }
             );
             break;
           } else if (field === "country") {
+            this.isCategory = false;
+            this.isSearch = false;
             countryName = param.country;
           } else if (field === "category") {
+            this.isCategory = true;
+            this.isSearch = false;
+            this.categoryString = param.category;
             this._newsService.getTopics(param.category).subscribe(
               news => {
                 this._zone.run(() => {
                   this.news = news.articles;
                 });
+                this.isLoading = false;
               },
               error => {
                 if (error) {
                   console.log(error);
                 }
+                this.isLoading = false;
               }
             );
             break;
           }
         }
       } else {
+        this.isCategory = false;
+        this.isSearch = false;
         countryName = "us";
       }
       this._newsService.getAllNews(countryName).subscribe(
@@ -91,11 +109,13 @@ export class HomeComponent implements OnInit {
           this._zone.run(() => {
             this.news = news.articles;
           });
+          this.isLoading = false;
         },
         error => {
           if (error) {
             console.log(error.message);
           }
+          this.isLoading = false;
         }
       );
 
@@ -104,15 +124,16 @@ export class HomeComponent implements OnInit {
           this._zone.run(() => {
             this.topFour = topFourNews.articles;
           });
+          this.isLoading = false;
         },
         error => {
           if (error) {
             console.log(error.message);
           }
+          this.isLoading = false;
         }
       );
     });
-    this.isLoading = false;
   }
 
   categoryLink(category: string) {
